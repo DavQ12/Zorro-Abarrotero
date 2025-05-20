edb557c97d9a31973a89181ebb70c0d5b8ee0df4
jesus <fernandoramirez35@aragon.unam.mx>
2025-05-19 18:54:00 -0600

    merge

--- src/main/java/zorroAbarrotes/proyecto/ZorroAbarroteroApplication.java
+++ src/main/java/zorroAbarrotes/proyecto/ZorroAbarroteroApplication.java
@@ -2,10 +2,7 @@ package zorroAbarrotes.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.context.annotation.ComponentScan;
-=======
->>>>>>> e14e0612b723c46c91f7400f63c87b93ad4bf4a6
 
 @SpringBootApplication
 public class ZorroAbarroteroApplication {
